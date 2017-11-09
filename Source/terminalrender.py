08bdd8c86fb5c4d30fab5ecc9eb183c97b5404c5
Your Name <glanzer.markus@gmail.com>
2017-11-09 23:42:20 +0100

    baby steps; also: Fonts.

--- Source/terminalrender.py
+++ Source/terminalrender.py
@@ -1,15 +1,35 @@
 #! /bin/python
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
 from io import StringIO, BytesIO
+import os
 
-#dimensions = (512,512)
+dimensions = [512,512]
+colour = [0.0,0.0,0.0]
 
-img = Image.open("Testdata/image.jpg")
+#print(os.getcwd()+"/Fonts/NovaMono.ttf")
+
+font = ImageFont.truetype(os.getcwd()+"/Fonts/FN0T.TTF",12,0,"unic")
+#font = ImageFont.load_default()
+#img = Image.open("Testdata/image.jpg")
+img = Image.new("RGB",dimensions)
+
+draw = ImageDraw.Draw(img)
+
+draw.text((10,10), "This is some kind of test.\nMONOTYPE. MONOTYPE! Monotype.\n$:", font=font)
 
 ibuffer = BytesIO()
 img.save(ibuffer, "PNG")
 umg = Image.open(ibuffer)
+
+draw = ImageDraw.Draw(umg)
+
+font = ImageFont.truetype(os.getcwd()+"/Fonts/NovaMono.ttf", 12,0,"unic")
+
+draw.text((10,48), "NOVAMONO:novamono\n$:", font=font, fill=(255,100,200,128))
+
 umg.show()
 
-# initialize array for the spritesheet and find out if just storing all the values is more efficient than parsing the spritesheet anew every time.
+# ImageFont does the trick for fonts.
+# figure out alpha-channels
+# figure out effects, animations and stuff to render proper terminals.
